0d3276146371d54aa0837a3a02bcec48664fd3ef
Jesus Reyes <47608293+reyesjd@users.noreply.github.com>
2022-04-23 15:11:29 -0500

    Add new post, get post by id, get user posts and get recent post features added

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,19 +1,16 @@
 import { Router } from "express";
+import {
+  getRecentPost,
+  getPosts,
+  createPost,
+} from "../controllers/post.controller.js";
 
 const router = Router();
 
-router.get("/:user_id", (req, res) => {
-  const { user_id } = req.params;
-});
+router.get("/recent", getRecentPost);
 
-router.get("/recent", (req, res) => {});
+router.get("/", getPosts);
 
-router.get("/", (req, res) => {
-  const { post_id } = req.params;
-});
-
-router.post("/", (req, res) => {
-  const { owner_id, img_url, display_name, description, price } = req.body;
-});
+router.post("/", createPost);
 
 export default router;
